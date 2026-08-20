a7e001de2f1425cfd879b65176cfa39a67dde433
aaaxgpf <zhangyx24@mails.tsinghua.edu.cn>
2026-08-21 01:52:30 +0800

    Update vocabulary.ts

--- src/data/vocabulary.ts
+++ src/data/vocabulary.ts
@@ -1,589 +1,292 @@
-import { VocabItem } from '../types';
+export interface VocabItem {
+  id: string;
+  category: string; // 对应分类标签
+  korean: string;
+  hanja?: string;
+  pronunciation: string;
+  chinese: string;
+  exampleKorean: string;
+  exampleChinese: string;
+}
 
-export const INITIAL_VOCABULARY: VocabItem[] = [
-  {
-    id: 'vocab_yonsei_0',
-    word: '안녕하십니까',
-    hangul: '안녕하십니까',
-    type: '表达',
-    meaning_zh: '你好',
-    meaning_en: 'hello; how do you do?',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_1',
-    word: '이름',
-    hangul: '이름',
-    type: '名',
-    meaning_zh: '姓名',
-    meaning_en: 'name; full name',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_2',
-    word: '선생님',
-    hangul: '선생님',
-    type: '名',
-    meaning_zh: '老师',
-    meaning_en: 'teacher',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_3',
-    word: '친구',
-    hangul: '친구',
-    type: '名',
-    meaning_zh: '朋友',
-    meaning_en: 'friend',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_4',
-    word: '어느',
-    hangul: '어느',
-    type: '冠',
-    meaning_zh: '哪(个)',
-    meaning_en: 'any',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_5',
-    word: '나라',
-    hangul: '나라',
-    type: '名',
-    meaning_zh: '国家',
-    meaning_en: 'country; nation',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_6',
-    word: '사람',
-    hangul: '사람',
-    type: '名',
-    meaning_zh: '人',
-    meaning_en: 'human; man',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_7',
-    word: '한국',
-    hangul: '한국',
-    type: '名',
-    meaning_zh: '韩国',
-    meaning_en: 'Korea',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_8',
-    word: '중국',
-    hangul: '중국',
-    type: '名',
-    meaning_zh: '中国',
-    meaning_en: 'China',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_9',
-    word: '회사원',
-    hangul: '회사원',
-    type: '名',
-    meaning_zh: '公司职员',
-    meaning_en: 'employee; office worker',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_10',
-    word: '학생',
-    hangul: '학생',
-    type: '名',
-    meaning_zh: '学生',
-    meaning_en: 'student',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_11',
-    word: '만나다',
-    hangul: '만나다',
-    type: '动',
-    meaning_zh: '见面',
-    meaning_en: 'meet',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_12',
-    word: '인사하다',
-    hangul: '인사하다',
-    type: '动',
-    meaning_zh: '打招呼',
-    meaning_en: 'greet',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_13',
-    word: '학교',
-    hangul: '학교',
-    type: '名',
-    meaning_zh: '学校',
-    meaning_en: 'school',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_14',
-    word: '집',
-    hangul: '집',
-    type: '名',
-    meaning_zh: '家',
-    meaning_en: 'house',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_15',
-    word: '교과서',
-    hangul: '교과서',
-    type: '名',
-    meaning_zh: '课本',
-    meaning_en: 'textbook',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_16',
-    word: '사전',
-    hangul: '사전',
-    type: '名',
-    meaning_zh: '词典',
-    meaning_en: 'dictionary',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_17',
-    word: '부탁하다',
-    hangul: '부탁하다',
-    type: '动',
-    meaning_zh: '拜托',
-    meaning_en: 'request',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_18',
-    word: '도움',
-    hangul: '도움',
-    type: '名',
-    meaning_zh: '帮助',
-    meaning_en: 'help',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_19',
-    word: '필요하다',
-    hangul: '필요하다',
-    type: '形',
-    meaning_zh: '需要',
-    meaning_en: 'necessary',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_20',
-    word: '졸업하다',
-    hangul: '졸업하다',
-    type: '动',
-    meaning_zh: '毕业',
-    meaning_en: 'graduate',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_21',
-    word: '전공',
-    hangul: '전공',
-    type: '名',
-    meaning_zh: '专业',
-    meaning_en: 'major; specialty',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_22',
-    word: '경영학',
-    hangul: '경영학',
-    type: '名',
-    meaning_zh: '经营学',
-    meaning_en: 'business administration',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_23',
-    word: '설렁탕',
-    hangul: '설렁탕',
-    type: '名',
-    meaning_zh: '牛杂碎汤',
-    meaning_en: 'ox bone soup',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_24',
-    word: '반찬',
-    hangul: '반찬',
-    type: '名',
-    meaning_zh: '菜',
-    meaning_en: 'side dish',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_25',
-    word: '접시',
-    hangul: '접시',
-    type: '名',
-    meaning_zh: '碟子',
-    meaning_en: 'plate',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_26',
-    word: '계란프라이',
-    hangul: '계란프라이',
-    type: '表达',
-    meaning_zh: '煎鸡蛋',
-    meaning_en: 'fried egg',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_27',
-    word: '고추장',
-    hangul: '고추장',
-    type: '名',
-    meaning_zh: '辣椒酱',
-    meaning_en: 'red chili paste',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_28',
-    word: '시간을 내다',
-    hangul: '시간을 내다',
-    type: '表达',
-    meaning_zh: '腾出时间',
-    meaning_en: 'make time',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_29',
-    word: '마음을 먹다',
-    hangul: '마음을 먹다',
-    type: '表达',
-    meaning_zh: '下决心',
-    meaning_en: 'make up one\'s mind',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_30',
-    word: '가능하다',
-    hangul: '가능하다',
-    type: '形',
-    meaning_zh: '可能',
-    meaning_en: 'possible; able',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_31',
-    word: '모으다',
-    hangul: '모으다',
-    type: '动',
-    meaning_zh: '收集',
-    meaning_en: 'collect',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_32',
-    word: '세계',
-    hangul: '세계',
-    type: '名',
-    meaning_zh: '世界',
-    meaning_en: 'world',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_33',
-    word: '거의',
-    hangul: '거의',
-    type: '副',
-    meaning_zh: '几乎',
-    meaning_en: 'nearly',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_34',
-    word: '수집하다',
-    hangul: '수집하다',
-    type: '动',
-    meaning_zh: '收集',
-    meaning_en: 'collect',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_35',
-    word: '감상하다',
-    hangul: '감상하다',
-    type: '动',
-    meaning_zh: '欣赏',
-    meaning_en: 'appreciate',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_36',
-    word: '다양하다',
-    hangul: '다양하다',
-    type: '形',
-    meaning_zh: '各种各样',
-    meaning_en: 'various',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_37',
-    word: '겁',
-    hangul: '겁',
-    type: '名',
-    meaning_zh: '害怕',
-    meaning_en: 'fear; fright',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_38',
-    word: '적응',
-    hangul: '적응',
-    type: '名',
-    meaning_zh: '适应',
-    meaning_en: 'adaptation',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_39',
-    word: '다행이다',
-    hangul: '다행이다',
-    type: '表达',
-    meaning_zh: '万幸',
-    meaning_en: 'be fortunate',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_40',
-    word: '시설',
-    hangul: '시설',
-    type: '名',
-    meaning_zh: '设施',
-    meaning_en: 'facility',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_41',
-    word: '도심',
-    hangul: '도심',
-    type: '名',
-    meaning_zh: '城市中心',
-    meaning_en: 'downtown',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_42',
-    word: '번화하다',
-    hangul: '번화하다',
-    type: '形',
-    meaning_zh: '繁华',
-    meaning_en: 'bustling',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_43',
-    word: '쾌적하다',
-    hangul: '쾌적하다',
-    type: '形',
-    meaning_zh: '舒适',
-    meaning_en: 'pleasant',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_44',
-    word: '인생',
-    hangul: '인생',
-    type: '名',
-    meaning_zh: '人生',
-    meaning_en: 'life',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_45',
-    word: '영향',
-    hangul: '영향',
-    type: '名',
-    meaning_zh: '影响',
-    meaning_en: 'influence',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_46',
-    word: '통계청',
-    hangul: '통계청',
-    type: '名',
-    meaning_zh: '统计厅',
-    meaning_en: 'Statistics Korea',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_47',
-    word: '경쟁',
-    hangul: '경쟁',
-    type: '名',
-    meaning_zh: '竞争',
-    meaning_en: 'competition',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_48',
-    word: '뚫다',
-    hangul: '뚫다',
-    type: '动',
-    meaning_zh: '穿',
-    meaning_en: 'dig; pierce',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_49',
-    word: '고려하다',
-    hangul: '고려하다',
-    type: '动',
-    meaning_zh: '考虑',
-    meaning_en: 'consider',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_50',
-    word: '가치관',
-    hangul: '가치관',
-    type: '名',
-    meaning_zh: '价值观',
-    meaning_en: 'values',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_51',
-    word: '소신',
-    hangul: '소신',
-    type: '名',
-    meaning_zh: '信念',
-    meaning_en: 'belief',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_52',
-    word: '보람',
-    hangul: '보람',
-    type: '名',
-    meaning_zh: '意义',
-    meaning_en: 'worth',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_53',
-    word: '유형',
-    hangul: '유형',
-    type: '名',
-    meaning_zh: '类型',
-    meaning_en: 'type',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_54',
-    word: '언어 능력',
-    hangul: '언어 능력',
-    type: '表达',
-    meaning_zh: '语言能力',
-    meaning_en: 'language proficiency',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_55',
-    word: '수장',
-    hangul: '수장',
-    type: '名',
-    meaning_zh: '首长',
-    meaning_en: 'head; chief',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_56',
-    word: '소감',
-    hangul: '소감',
-    type: '名',
-    meaning_zh: '感想',
-    meaning_en: 'thoughts',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_57',
-    word: '한결같다',
-    hangul: '한결같다',
-    type: '形',
-    meaning_zh: '始终如一',
-    meaning_en: 'constant; steadfast',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_58',
-    word: '지지',
-    hangul: '지지',
-    type: '名',
-    meaning_zh: '支持',
-    meaning_en: 'support',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_59',
-    word: '성원',
-    hangul: '성원',
-    type: '名',
-    meaning_zh: '成员',
-    meaning_en: 'member',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_60',
-    word: '귀감',
-    hangul: '귀감',
-    type: '名',
-    meaning_zh: '榜样',
-    meaning_en: 'role model',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_61',
-    word: '막연히',
-    hangul: '막연히',
-    type: '副',
-    meaning_zh: '茫然',
-    meaning_en: 'vaguely',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_62',
-    word: '누비다',
-    hangul: '누비다',
-    type: '动',
-    meaning_zh: '穿行',
-    meaning_en: 'move around',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_63',
-    word: '구체화되다',
-    hangul: '구체화되다',
-    type: '动',
-    meaning_zh: '具体化',
-    meaning_en: 'be concretized',
-    source: 'Yonsei Korean'
-  },
-  {
-    id: 'vocab_yonsei_64',
-    word: '시련',
-    hangul: '시련',
-    type: '名',
-    meaning_zh: '考验',
-    meaning_en: 'trial',
-    source: 'Yonsei Korean'
-  },
+export const VOCAB_CATEGORIES = [
+  'All Lists',
+  'TOPIK Beginner',
+  'TOPIK Intermediate',
+  'TOPIK Advanced',
+  'Yonsei Vocab',
+  'SNU Vocab',
+  'K-POP Vocab',
+  'Daily Vocab'
+] as const;
+
+export const VOCABULARY_LIST: VocabItem[] = [
+  // 1. Daily Vocab
+  {
+    id: 'v_daily_1',
+    category: 'Daily Vocab',
+    korean: '약속',
+    hanja: '約束',
+    pronunciation: '[약쏙]',
+    chinese: '约定、约会',
+    exampleKorean: '오늘 저녁에 친구랑 약속이 있어요.',
+    exampleChinese: '今天晚上我和朋友有约。'
+  },
+  {
+    id: 'v_daily_2',
+    category: 'Daily Vocab',
+    korean: '퇴근',
+    hanja: '退勤',
+    pronunciation: '[퇴ː근]',
+    chinese: '下班',
+    exampleKorean: '몇 시에 퇴근하세요?',
+    exampleChinese: '您几点下班？'
+  },
+  {
+    id: 'v_daily_3',
+    category: 'Daily Vocab',
+    korean: '산책',
+    hanja: '散策',
+    pronunciation: '[산책]',
+    chinese: '散步',
+    exampleKorean: '날씨가 좋아서 공원에서 산책했어요.',
+    exampleChinese: '天气很好，所以在公园散了步。'
+  },
+  {
+    id: 'v_daily_4',
+    category: 'Daily Vocab',
+    korean: '배달',
+    hanja: '配達',
+    pronunciation: '[배달]',
+    chinese: '外卖、配送',
+    exampleKorean: '저녁으로 치킨을 배달시켰어요.',
+    exampleChinese: '晚餐点了炸鸡外卖。'
+  },
+  {
+    id: 'v_daily_5',
+    category: 'Daily Vocab',
+    korean: '충전',
+    hanja: '充電',
+    pronunciation: '[충전]',
+    chinese: '充电、充值',
+    exampleKorean: '휴대폰 배터리를 충전해야 해요.',
+    exampleChinese: '手机得充电了。'
+  },
+
+  // 2. TOPIK Beginner
+  {
+    id: 'v_beg_1',
+    category: 'TOPIK Beginner',
+    korean: '선생님',
+    pronunciation: '[선생님]',
+    chinese: '老师',
+    exampleKorean: '선생님, 질문이 있습니다.',
+    exampleChinese: '老师，我有一个问题。'
+  },
+  {
+    id: 'v_beg_2',
+    category: 'TOPIK Beginner',
+    korean: '도서관',
+    hanja: '圖書館',
+    pronunciation: '[도서관]',
+    chinese: '图书馆',
+    exampleKorean: '주말에 도서관에서 공부해요.',
+    exampleChinese: '周末在图书馆学习。'
+  },
+  {
+    id: 'v_beg_3',
+    category: 'TOPIK Beginner',
+    korean: '식당',
+    hanja: '食堂',
+    pronunciation: '[식땅]',
+    chinese: '餐厅、食堂',
+    exampleKorean: '학교 앞 식당에서 밥을 먹었어요.',
+    exampleChinese: '在学校前面的餐厅吃了饭。'
+  },
+  {
+    id: 'v_beg_4',
+    category: 'TOPIK Beginner',
+    korean: '감사하다',
+    hanja: '感謝--',
+    pronunciation: '[감사하다]',
+    chinese: '感谢',
+    exampleKorean: '도와주셔서 진심으로 감사합니다.',
+    exampleChinese: '非常感谢您的帮助。'
+  },
+  {
+    id: 'v_beg_5',
+    category: 'TOPIK Beginner',
+    korean: '사과',
+    pronunciation: '[사과]',
+    chinese: '苹果 / 道歉',
+    exampleKorean: '맛있는 사과를 샀어요.',
+    exampleChinese: '买了美味的苹果。'
+  },
+
+  // 3. TOPIK Intermediate
+  {
+    id: 'v_int_1',
+    category: 'TOPIK Intermediate',
+    korean: '경험',
+    hanja: '經驗',
+    pronunciation: '[경험]',
+    chinese: '经验、经历',
+    exampleKorean: '다양한 경험을 쌓는 것이 중요합니다.',
+    exampleChinese: '积累多样化的经验非常重要。'
+  },
+  {
+    id: 'v_int_2',
+    category: 'TOPIK Intermediate',
+    korean: '환경',
+    hanja: '環境',
+    pronunciation: '[환경]',
+    chinese: '环境',
+    exampleKorean: '자연환경을 보호해야 합니다.',
+    exampleChinese: '我们必须保护自然环境。'
+  },
+  {
+    id: 'v_int_3',
+    category: 'TOPIK Intermediate',
+    korean: '성공',
+    hanja: '成功',
+    pronunciation: '[성공]',
+    chinese: '成功',
+    exampleKorean: '노력 끝에 프로젝트를 성공시켰다.',
+    exampleChinese: '经过努力终于使项目获得了成功。'
+  },
+  {
+    id: 'v_int_4',
+    category: 'TOPIK Intermediate',
+    korean: '결과',
+    hanja: '結果',
+    pronunciation: '[결과]',
+    chinese: '结果',
+    exampleKorean: '좋은 결과를 기대하고 있어요.',
+    exampleChinese: '正期待着好的结果。'
+  },
+  {
+    id: 'v_int_5',
+    category: 'TOPIK Intermediate',
+    korean: '발전',
+    hanja: '發展',
+    pronunciation: '[발쩐]',
+    chinese: '发展、进步',
+    exampleKorean: '한국어 실력이 많이 발전했어요.',
+    exampleChinese: '韩语水平进步了许多。'
+  },
+
+  // 4. TOPIK Advanced
+  {
+    id: 'v_adv_1',
+    category: 'TOPIK Advanced',
+    korean: '불가피하다',
+    hanja: '不可避--',
+    pronunciation: '[불가피하다]',
+    chinese: '不可避免的',
+    exampleKorean: '일정 변경이 불가피한 상황입니다.',
+    exampleChinese: '日程变更处于不可避免的情况。'
+  },
+  {
+    id: 'v_adv_2',
+    category: 'TOPIK Advanced',
+    korean: '추진하다',
+    hanja: '推進--',
+    pronunciation: '[추진하다]',
+    chinese: '推进、推动',
+    exampleKorean: '새로운 정책을 적극적으로 추진하고 있다.',
+    exampleChinese: '正在积极推进新政策。'
+  },
+  {
+    id: 'v_adv_3',
+    category: 'TOPIK Advanced',
+    korean: '타협',
+    hanja: '妥協',
+    pronunciation: '[타협]',
+    chinese: '妥协',
+    exampleKorean: '양측은 오랜 협상 끝에 타협점에 도달했다.',
+    exampleChinese: '双方经过漫长谈判达成了妥协点。'
+  },
+
+  // 5. K-POP Vocab
+  {
+    id: 'v_kpop_1',
+    category: 'K-POP Vocab',
+    korean: '컴백',
+    pronunciation: '[컴백]',
+    chinese: '回归 (Comeback)',
+    exampleKorean: '다음 달에 정규 앨범으로 컴백해요!',
+    exampleChinese: '下个月带着正规专辑回归！'
+  },
+  {
+    id: 'v_kpop_2',
+    category: 'K-POP Vocab',
+    korean: '음방',
+    pronunciation: '[음방]',
+    chinese: '音乐打歌节目 (음악방송)',
+    exampleKorean: '오늘 음방 1위 후보에 올랐어요.',
+    exampleChinese: '今天登上了音放一位候补。'
+  },
+  {
+    id: 'v_kpop_3',
+    category: 'K-POP Vocab',
+    korean: '팬싸',
+    pronunciation: '[팬싸]',
+    chinese: '粉丝签名会 (팬사인회)',
+    exampleKorean: '주말에 대면 팬싸에 다녀왔어.',
+    exampleChinese: '周末去了线下签售会。'
+  },
+  {
+    id: 'v_kpop_4',
+    category: 'K-POP Vocab',
+    korean: '응원봉',
+    hanja: '應援棒',
+    pronunciation: '[응원봉]',
+    chinese: '应援棒',
+    exampleKorean: '콘서트에 응원봉을 챙겨갔어요.',
+    exampleChinese: '带了应援棒去看演唱会。'
+  },
+
+  // 6. Yonsei Vocab
+  {
+    id: 'v_yonsei_1',
+    category: 'Yonsei Vocab',
+    korean: '수강신청',
+    hanja: '受講申請',
+    pronunciation: '[수강신청]',
+    chinese: '选课',
+    exampleKorean: '내일 아침 9시에 수강신청이 시작됩니다.',
+    exampleChinese: '明天上午9点开始选课。'
+  },
+  {
+    id: 'v_yonsei_2',
+    category: 'Yonsei Vocab',
+    korean: '장학금',
+    hanja: '奬學金',
+    pronunciation: '[장학끔]',
+    chinese: '奖学金',
+    exampleKorean: '이번 학기에 성적 장학금을 받았어요.',
+    exampleChinese: '这学期拿到了成绩奖学金。'
+  },
+
+  // 7. SNU Vocab
+  {
+    id: 'v_snu_1',
+    category: 'SNU Vocab',
+    korean: '학술대회',
+    hanja: '學術大會',
+    pronunciation: '[학쑬대회]',
+    chinese: '学术研讨会',
+    exampleKorean: '국제 학술대회에서 논문을 발표했습니다.',
+    exampleChinese: '在国际学术研讨会上发表了论文。'
+  },
+  {
+    id: 'v_snu_2',
+    category: 'SNU Vocab',
+    korean: '과제',
+    hanja: '課題',
+    pronunciation: '[과제]',
+    chinese: '课题、作业',
+    exampleKorean: '오늘 밤까지 제출해야 하는 과제가 있어요.',
+    exampleChinese: '有今晚之前必须提交的课题作业。'
+  }
 ];
